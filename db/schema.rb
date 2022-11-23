1b7e1aae23ef142684ada769f3dc1ed3286afff5
Joseph Garrett <josephgarrett243@gmail.com>
2022-11-23 23:19:07 +0000

    Fixed migration issues

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_23_172634) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_18_020403) do
   create_table "projects", force: :cascade do |t|
     t.string "title"
     t.text "description"
