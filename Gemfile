f015380641c52a56ebb3a39cd0a783eb1ed841bf
Joseph Garrett <josephgarrett243@gmail.com>
2022-12-02 04:57:45 +0000

    Integrated bootstrap and made the home page look nice

--- Gemfile
+++ Gemfile
@@ -7,6 +7,8 @@ gem 'devise'
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.0.3"
 
+gem 'bootstrap', '~> 5.2.2'
+
 # The original asset pipeline for Rails [https://github.com/rails/sprockets-rails]
 gem "sprockets-rails"
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "projects#index"
-  resources :projects
+    root "projects#index"
+    resources :projects
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
 end
 
   # Defines the root path route ("/")
